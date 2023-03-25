d64640e958bc42d4ef891d31df67105b92d80130
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2023-03-25 09:31:08 +0300

    wrong init project

--- src/data.ts
+++
@@ -1,9 +0,0 @@
-const data = [
-  { id: 1, title: "Item 1" },
-  { id: 2, title: "Item 2" },
-  { id: 3, title: "Item 3" },
-  { id: 4, title: "Item 4" },
-  { id: 5, title: "Item 5" },
-];
-
-export default data;
