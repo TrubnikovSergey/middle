aae3771ac26bef4497bde5c334eca59fc63562b7
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2023-04-17 12:34:35 +0300

    mockData.js changed on mockData.json and import mockData fixed

--- TS_task3/task3.js
+++ TS_task3/task3.js
@@ -1,13 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const mockData_1 = require("./mockData");
+const mockData_json_1 = __importDefault(require("./mockData.json"));
 const normalizeData = (unnormalizedData) => {
     const byId = unnormalizedData.reduce((acc, item) => (Object.assign(Object.assign({}, acc), { [item.id]: item })), {});
     const allIds = unnormalizedData.map((item) => item.id);
     const result = { byId, allIds };
     return result;
 };
-console.log(normalizeData(mockData_1.posts));
+console.log(normalizeData(mockData_json_1.default));
 /**
  * {
  *    byId: {
