b97721f3de5a4ff2a60236e0996d745b0b2f95d5
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2023-04-19 19:24:11 +0300

    add zeroing states in hook usefetch

--- hooks/usefetch/src/useFetch.js
+++ hooks/usefetch/src/useFetch.js
@@ -4,31 +4,53 @@ import { useEffect, useState } from "react";
 const http = axios.create();
 
 const useFetch = (url) => {
-  // data, isLoading, error, refetch
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(Boolean(url));
   const [error, setError] = useState(null);
 
+  function catchError(error) {
+    setIsLoading(false);
+    setData(null);
+    setError(error);
+  }
+
+  function getRespons(data) {
+    setIsLoading(false);
+    setData(data);
+    setError(null);
+  }
+
+  function resetData() {
+    setIsLoading(true);
+    setError(null);
+    setData(null);
+  }
+
   const refetch = (arg) => {
+    resetData();
+
     if (arg?.params?._limit) {
       http
         .get(url, { params: arg.params })
         .then((resp) => {
-          setIsLoading(false);
-          setData(resp.data);
+          getRespons(resp.data);
         })
-        .catch((error) => setError(error));
+        .catch((error) => {
+          catchError(error);
+        });
     }
   };
 
   useEffect(() => {
+    resetData();
     http
       .get(url)
       .then((resp) => {
-        setIsLoading(false);
-        setData(resp.data);
+        getRespons(resp.data);
       })
-      .catch((error) => setError(error));
+      .catch((error) => {
+        catchError(error);
+      });
   }, []);
 
   return { data, isLoading, error, refetch };
