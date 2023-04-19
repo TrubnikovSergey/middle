8467b8b45e041154fecc1291fea313ad262c3565
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2023-04-19 10:58:05 +0300

    after pull

---
+++ TS_task3/task3.ts
@@ -0,0 +1,22 @@
+import posts from "./mockData.json";
+import { Post, ById, Normalize } from "./interfaces";
+
+const normalizeData = (unnormalizedData: Post[]): Normalize => {
+  const byId: ById = unnormalizedData.reduce((acc, item) => ({ ...acc, [item.id]: item }), {});
+  const allIds: string[] = unnormalizedData.map((item) => item.id);
+  const result: Normalize = { byId, allIds };
+
+  return result;
+};
+
+console.log(normalizeData(posts));
+/**
+ * {
+ *    byId: {
+ *      62e69d5a5458aac0ed320b35: { id: '...', title: '...', body: '...' },
+ *      62e69d5a5458aac0ed320b1c: { id: '...', title: '...', body: '...' },
+ *      ...
+ *    },
+ *    allIds: ['62e69d5a5458aac0ed320b35', '62e69d5a5458aac0ed320b1c', ...]
+ * }
+ */
