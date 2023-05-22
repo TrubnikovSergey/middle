04965e6d5292fc19f55c785116b27a074b2507c5
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2023-05-22 21:00:17 +0300

    home work finished

---
+++ context/task1/src/pages/logout.tsx
@@ -0,0 +1,15 @@
+import { Link, Navigate } from "react-router-dom";
+import { useAuthContext } from "../components/authProvider";
+import { useEffect } from "react";
+
+const Logout = () => {
+  const auth = useAuthContext();
+
+  useEffect(() => {
+    return () => auth.logout();
+  }, []);
+
+  return <Navigate to="/" />;
+};
+
+export default Logout;
